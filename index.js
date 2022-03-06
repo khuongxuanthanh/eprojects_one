1dd09a7a3ebf5303d6c600a8f8313022c5ba4bf9
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-03-06 18:23:15 +0700

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const morgan = require('morgan');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('combined'));
 app.use(expressLayouts);
